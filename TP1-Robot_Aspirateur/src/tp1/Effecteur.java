38d2640dd2b5e1554720675dcba75be3acba9468
davidginesy <david.ginesy@gmail.com>
2017-09-27 18:37:50 -0400

    maj environnement et effecteur

--- TP1-Robot_Aspirateur/src/tp1/Effecteur.java
+++ TP1-Robot_Aspirateur/src/tp1/Effecteur.java
@@ -2,12 +2,13 @@ package tp1;
 
 public class Effecteur {
 	static void aspire() {
-		
+		Environnement.testAspire();
 	}
 	static void ramasse() {
-		
+		Environnement.testRamasse();
 	}
-	static void bouge() {
-		
+	static void bouge(int x, int y) {
+		Environnement.setX(x);
+		Environnement.setY(y);
 	}
 }
--- TP1-Robot_Aspirateur/src/tp1/Environnement.java
+++ TP1-Robot_Aspirateur/src/tp1/Environnement.java
@@ -10,10 +10,14 @@ public class Environnement implements Runnable {
 	private static int poussiere = 1;
 	private static int bijoux = 2;
 	private static int performance = 0;
+	private static int x = 0;
+	private static int y = 0;
 	
 	public void run() {
 		while(true) {
 		genererElement(carte);
+		//testAspire();
+		//testRamasse();
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
@@ -22,11 +26,30 @@ public class Environnement implements Runnable {
 		}
 		}
 	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public static void setX(int x) {
+		Environnement.x=x;
+	}
+	
+	public static void setY(int y) {
+		Environnement.y=y;
+	}
+	
 	public void genererElement(int[][] carte) {
 		Random ChoixCase = new Random();
 		int nb = ChoixCase.nextInt(10);
 		Random ChoixCase2 = new Random();
 		int nb2 = ChoixCase2.nextInt(10);
+		//setX(nb);
+		//setY(nb2);
 		Random element = new Random();
 		int elem = element.nextInt(2)+1;
 		if(carte[nb][nb2]==0) {
@@ -36,12 +59,54 @@ public class Environnement implements Runnable {
 			carte[nb][nb2]=3;
 		}
 		
-		//System.out.println(Arrays.deepToString(carte));
+		System.out.println(Arrays.deepToString(carte));
 		
 	}
 	public static int[][] voirCarte() {
 		return carte;
 	}
+	
+	public static void testAspire() {
+		switch(carte[x][y]) {
+		case 0 :
+			System.out.println("Aspire case vide");
+			break;
+		case 1 :
+			System.out.println("bravo! poussiere ramassée");
+			carte[x][y]=0;
+			break;
+		case 2 :
+			System.out.println("bijoux aspiré :/");
+			carte[x][y]=0;
+			break;
+		case 3 :
+			System.out.println("de la poussiere et des bijoux ont été aspirés :/");
+			carte[x][y]=0;
+			break;
+		}
+	}
+	
+	public static void testRamasse() {
+		switch(carte[x][y]) {
+		case 0 :
+			System.out.println("Ramasse case vide");
+			break;
+		case 1 :
+			System.out.println("ramassage de poussiere :/");
+			carte[x][y]=0;
+			break;
+		case 2 :
+			System.out.println("bravo! bijoux ramassé");
+			carte[x][y]=0;
+			break;
+		case 3 :
+			System.out.println("bravo! bijoux ramassé mais il reste de la poussiere");
+			carte[x][y]=1;
+			break;
+		}
+	}
+	
+	
 	/*public static void main(String[] args) {
 		 
 		genererElement(carte);
